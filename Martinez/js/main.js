f7b2e68328282ea489c30241f98a40fe53c6b25b
Keremcan Çakar <keremcan@hotmail.com.tr>
2017-05-08 21:59:12 +0300

    about
    d

--- Martinez/js/main.js
+++ Martinez/js/main.js
@@ -34,6 +34,7 @@ $(document).ready(function(){
 			$(this).addClass("underline");
 		}
 	});
+	$("body").show();
 });
 
 !function(e){e.fn.endlessScroll=function(l){var t={bottomPixels:50,fireOnce:!0,fireDelay:150,loader:"<br />Loading...<br />",data:"",insertAfter:"div:last",resetCounter:function(){return!1},callback:function(){return!0},ceaseFire:function(){return!1}},l=e.extend({},t,l),r=!0,i=!1,n=0;l.ceaseFire.apply(this)===!0&&(r=!1),r===!0&&e(this).scroll(function(){if(l.ceaseFire.apply(this)===!0)return void(r=!1);if(this==document||this==window)var t=e(document).height()-e(window).height()<=e(window).scrollTop()+l.bottomPixels;else{var o=e(".endless_scroll_inner_wrap",this);0==o.length&&(o=e(this).wrapInner('<div class="endless_scroll_inner_wrap" />').find(".endless_scroll_inner_wrap"));var t=o.length>0&&o.height()-e(this).height()<=e(this).scrollTop()+l.bottomPixels}t&&(0==l.fireOnce||1==l.fireOnce&&1!=i)&&(l.resetCounter.apply(this)===!0&&(n=0),i=!0,n++,e(l.insertAfter).after('<div id="endless_scroll_loader">'+l.loader+"</div>"),data="function"==typeof l.data?l.data.apply(this,[n]):l.data,data!==!1&&(e(l.insertAfter).after('<div id="endless_scroll_data">'+data+"</div>"),e("div#endless_scroll_data").hide().fadeIn(),e("div#endless_scroll_data").removeAttr("id"),l.callback.apply(this,[n]),l.fireDelay!==!1||0!==l.fireDelay?(e("body").after('<div id="endless_scroll_marker"></div>'),e("div#endless_scroll_marker").fadeTo(l.fireDelay,1,function(){e(this).remove(),i=!1})):i=!1),e("div#endless_scroll_loader").remove())})}}(jQuery),$(document).ready(function(){
@@ -71,7 +72,7 @@ $(document).endlessScroll(
 	}
 });
 
-$("body").show();
+
 
 document.body.scrollTop=document.documentElement.scrollTop=0;
 
