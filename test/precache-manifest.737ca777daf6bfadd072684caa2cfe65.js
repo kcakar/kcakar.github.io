f6288e56f7f11929fe08d570f886959a761e5621
Keremcan Çakar <keremcan@hotmail.com.tr>
2018-11-28 00:02:15 +0300

    fx

---
+++ test/precache-manifest.737ca777daf6bfadd072684caa2cfe65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bafcdb4c1122c24daf28",
+    "url": "/test/static/css/main.21e3de53.chunk.css"
+  },
+  {
+    "revision": "bafcdb4c1122c24daf28",
+    "url": "/test/static/js/main.bafcdb4c.chunk.js"
+  },
+  {
+    "revision": "faf97dadff339d401f5d",
+    "url": "/test/static/css/1.f14ad6f1.chunk.css"
+  },
+  {
+    "revision": "faf97dadff339d401f5d",
+    "url": "/test/static/js/1.faf97dad.chunk.js"
+  },
+  {
+    "revision": "5ad1e219c88e053e0b1e",
+    "url": "/test/static/js/runtime~main.5ad1e219.js"
+  },
+  {
+    "revision": "1ee4ab2dd471872a6c86b44d46f2c6ce",
+    "url": "/test/index.html"
+  }
+];
--- test/service-worker.js
+++ test/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.18f3706d7f75e25c26a0e66ca72b29c0.js"
+  "/test/precache-manifest.737ca777daf6bfadd072684caa2cfe65.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/test/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
