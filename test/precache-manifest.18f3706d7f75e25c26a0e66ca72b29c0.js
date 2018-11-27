0be8aad4e2fc630a70b3c6058fca1ca2d5e2ccb1
Keremcan Çakar <keremcan@hotmail.com.tr>
2018-11-27 23:56:25 +0300

    test

---
+++ test/precache-manifest.18f3706d7f75e25c26a0e66ca72b29c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "270b9adb45165c6b9f54",
+    "url": "/static/css/main.21e3de53.chunk.css"
+  },
+  {
+    "revision": "270b9adb45165c6b9f54",
+    "url": "/static/js/main.270b9adb.chunk.js"
+  },
+  {
+    "revision": "faf97dadff339d401f5d",
+    "url": "/static/css/1.f14ad6f1.chunk.css"
+  },
+  {
+    "revision": "faf97dadff339d401f5d",
+    "url": "/static/js/1.faf97dad.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c1ab640d32f6bc0a754948b1d03013a2",
+    "url": "/index.html"
+  }
+];
