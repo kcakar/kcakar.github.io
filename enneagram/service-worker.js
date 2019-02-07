80df5d3516f9049987a903645a66e567f479320e
Keremcan Cakar <kcakar@apple.com>
2019-02-07 13:18:11 +0300

    link-fix

--- enneagram/service-worker.js
+++ enneagram/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/enneagram/precache-manifest.685fba9001f4d09bfe3e85f28b26e2c6.js"
+  "/enneagram/precache-manifest.92cb7728a63173496036e5d3b7a9f1f2.js"
 );
 
 workbox.clientsClaim();
