836f70e89f9e92d00b1000a75bf6d7b9f11421e0
Keremcan Çakar <keremcan@hotmail.com.tr>
2024-11-17 20:22:59 +0100

    optimize

--- rockbride/rockbride/js/app.js
+++ rockbride/rockbride/js/app.js
@@ -28,3 +28,31 @@ function init() {
 document.addEventListener("DOMContentLoaded", function() {
     init(); 
 });
+
+const lazyImages = document.querySelectorAll('.lazy-img');
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting || entry.intersectionRatio >= 0.3) {
+      // When the image is 30% in view (or slightly before), load the image
+      const img = entry.target;
+      img.src = img.dataset.src; // Set the actual image source from data-src
+
+      // Optionally, add a class to fade in the image once it's loaded
+      img.onload = () => {
+        img.classList.add('loaded'); // Fade in the image
+      };
+
+      // Stop observing the image once it's loaded
+      observer.unobserve(img);
+    }
+  });
+}, {
+  rootMargin: '300px 0px', // Preload 300px before entering the viewport
+  threshold: 0.1, // Trigger when 10% of the image is visible
+});
+
+lazyImages.forEach(img => {
+  observer.observe(img); // Observe each image
+});
+ 
